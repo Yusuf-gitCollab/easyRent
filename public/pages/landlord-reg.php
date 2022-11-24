fa9a03813eb0827db7443f4a6d403a49e5665056
Yusuf-GitCollab <hs34kumar@gmail.com>
2022-11-24 11:31:36 +0530

    landlord registration working fine. Both backend and frontend done

--- public/pages/landlord-reg.php
+++ public/pages/landlord-reg.php
@@ -38,23 +38,25 @@ if (session_status() === PHP_SESSION_NONE) {
   <section>
     <div class="container">
       <form action="../../server/register.php" method="POST" enctype="multipart/form-data">
-        <div class="landlord-account-wrapper">
+        <div class="landlord-account-wrapper"> <!---- contains different divs like personal details div, appartment details div etc --->
           <div class="account-header">
+            <!------------------------------ PROFILE IMAGE DIV -------------------------------------->
             <div
               class="profile-img <?php echo ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true)) ? 'view' : '' ?>">
 
-              <?php if ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == false)): ?>
-
+              <?php if ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == false)): ?> 
+              <!--- this is executed when the user is signed in -->
               <img
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkmQL4_XePw5_yIHPkzc0lfb8FesWP4TR9MQ&usqp=CAU"
                 alt="">
               <?php elseif ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == true)): ?>
-
+              <!-- this is executed when the user is loged in but wants to edit his / her profile -->
               <?php else: ?>
               <label for="fileToUpload"> Upload Image </label>
               <input type="file" name="fileToUpload" id="fileToUpload">
               <?php endif ?>
             </div>
+            <!---------------------------------------- PROFILE BASIC INFO ------------------------------------>
             <div class="profile-basic-info">
               <?php if ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == false)): ?>
               <h1> Cha Eun Woo </h1>
@@ -67,72 +69,145 @@ if (session_status() === PHP_SESSION_NONE) {
               <?php endif ?>
             </div>
           </div>
+
+          <!--------------------------------PERSONAL DETAILS DIV --------------------------------->
           <div class="personal-details-div">
-            <?php if ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == false)): ?>
+  
             <div class="input-wrapper">
               <div>
                 <label for="name">Email: </label>
-                <input class="view" type="text" placeholder="harsh1.5lacs@gmail.com" readonly>
+                <?php if ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == false)): ?>
+                  <input class="view" name="email_id" type="text" placeholder="harsh1.5lacs@gmail.com" readonly>
+                <?php elseif ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == true)): ?>
+                  
+                <?php else: ?>
+                  <input  type="text" name="email_id" placeholder="eg: xyz@abc.mail" required> 
+                <?php endif ?>
               </div>
+
               <div>
                 <label for="name">Phone: </label>
-                <input class="view" type="text" placeholder="6000295281" readonly>
+                <?php if ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == false)): ?>
+                  <input class="view" type="number" name="mobile_number" placeholder="6000295281" readonly>
+                <?php elseif ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == true)): ?>
+                  
+                <?php else: ?>
+                  <input  type="text" name="mobile_number" placeholder="Enter mobile number" required> 
+                <?php endif ?>
               </div>
-            </div>
+            </div> <!----- END OF INPUT WRAPPER contains two div in it for two input fields -------------->
 
             <div class="input-wrapper">
               <div>
                 <label for="name">Password: </label>
-                <input class="view" type="text" placeholder="deboHarsh@2022" readonly>
+                <?php if ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == false)): ?>
+                  <input class="view" name="password" type="password" placeholder="********" readonly>
+                <?php elseif ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == true)): ?>
+                  
+                <?php else: ?>
+                  <input  type="password" name="password" placeholder="Atleast 8 characters" required> 
+                <?php endif ?>
               </div>
-            </div>
-
-
-            <?php elseif ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == true)): ?>
-
-            <?php endif ?>
-          </div>
+            </div> <!---- END OF INPUT WRAPPER ------------------>
+          </div> <!--- END OF PERSONAL DETAILS DIV --------------->
 
           <div class="appartment-info-div">
-            <?php if ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == false)): ?>
+        
             <div class="input-wrapper">
               <div>
                 <label for="name">Appartment Name: </label>
-                <input class="view" type="text" placeholder="harsh1.5lacs@gmail.com" readonly>
+                <?php if ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == false)): ?>
+                  <input class="view" type="text" placeholder="harsh1.5lacs@gmail.com" readonly>
+                <?php elseif ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == true)): ?>
+                
+                <?php else: ?>
+                  <input type="text" name="app_name" placeholder="eg: Four Seasons" required>
+                <?php endif ?>
               </div>
               <div>
                 <label for="name">Appartment Number: </label>
-                <input class="view" type="text" placeholder="6000295281" readonly>
+                <?php if ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == false)): ?>
+                  <input class="view" type="text" placeholder="harsh1.5lacs@gmail.com" readonly>
+                <?php elseif ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == true)): ?>
+                
+                <?php else: ?>
+                  <input type="text" name="app_no" placeholder="eg: NE23" required>
+                <?php endif ?>
               </div>
-            </div>
+            </div> <!---------------- INPUT WRAPPER INSIDE Appartment INFO DIV ---------------->
 
             <div class="input-wrapper">
               <div>
                 <label for="name">Location: </label>
-                <input class="view" type="text" placeholder="harsh1.5lacs@gmail.com" readonly>
+                <?php if ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == false)): ?>
+                  <input class="view" type="text" placeholder="harsh1.5lacs@gmail.com" readonly>
+                <?php elseif ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == true)): ?>
+                
+                <?php else: ?>
+                  <input type="text" name="app_loc" placeholder="eg: Ulubari Road, Ghy-5" required>
+                <?php endif ?>
               </div>
               <div>
                 <label for="name">Appartment Rent: </label>
-                <input class="view" type="text" placeholder="6000295281" readonly>
+                <?php if ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == false)): ?>
+                  <input class="view" type="text" placeholder="harsh1.5lacs@gmail.com" readonly>
+                <?php elseif ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == true)): ?>
+                
+                <?php else: ?>
+                  <input type=number name="app_rent" placeholder="eg: $400" required>
+                <?php endif ?>
               </div>
-            </div>
+            </div>  <!---------------- INPUT WRAPPER INSIDE Appartment INFO DIV ---------------->
 
             <div class="input-wrapper">
               <div>
                 <label for="name">Appartment Type: </label>
-                <input class="view" type="text" placeholder="harsh1.5lacs@gmail.com" readonly>
+                <?php if ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == false)): ?>
+                  <input class="view" type="text" placeholder="harsh1.5lacs@gmail.com" readonly>
+                <?php elseif ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == true)): ?>
+                
+                <?php else: ?>
+                  <input type="text" name="app_type" placeholder="eg: 3BHK" required>
+                <?php endif ?>
               </div>
               <div>
-                <label for="name">Facilities: </label>
-                <input class="view" type="text" placeholder="6000295281" readonly>
+                <label for="name">Facilities: </label> <br/>
+                <?php if ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == false)): ?>
+                  <textarea class="view" type="text" placeholder="harsh1.5lacs@gmail.com" readonly> </textarea>
+                <?php elseif ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == true)): ?>
+                
+                <?php else: ?>
+                  <textarea type="text" name="app_fac" placeholder="eg: 2 Balconies, Hall" required> </textarea>
+                <?php endif ?>
               </div>
-            </div>
-            <?php elseif ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == true)): ?>
+            </div> <!---------------- INPUT WRAPPER INSIDE Appartment INFO DIV ---------------->
+      
+          </div> <!--- APPRATMENT INFO DIV ENDS ---------------->
 
+          <div class="appartment-photo-section">
+            <?php if ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == false)): ?> 
+              <!--- this is executed when the user is signed in -->
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkmQL4_XePw5_yIHPkzc0lfb8FesWP4TR9MQ&usqp=CAU"
+                alt="">
+              <?php elseif ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == true)): ?>
+              <!-- this is executed when the user is loged in but wants to edit his / her profile -->
+              <?php else: ?>
+              <label for="fileToUpload"> Upload Appartment Images </label>
+              <input type="file" name="files[]" id="fileToUpload" multiple required>
+            <?php endif ?>
+          </div>
+
+          <div class="button-div">
+            <?php if ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == false)): ?>
+              <input type="submit"  class="Edit" name="edit_landlord">
+            <?php elseif ((isset($_SESSION['logedin']) and $_SESSION['logedin'] == true) and (isset($_SESSION['edit-profile']) and $_SESSION['edit-profile'] == true)): ?>
+              <input type="submit" class="Save" name="resave_landlord">
+            <?php else: ?>
+              <input type="submit" value="Register" class="btn" name="reg_landlord">
             <?php endif ?>
           </div>
         </div> <!-- landlord-account-wrapper ends !-->
-        <input type="submit" class="btn" name="reg_landlord">
       </form>
     </div> <!-- Container div ends !-->
   </section>
--- server/register.php
+++ server/register.php
@@ -80,75 +80,127 @@ if(isset($_POST['login_user'])) {
 
 
 if(isset($_POST['reg_landlord'])) {
+    // this is for the landlord image which will be saved in landlord table
     $allowTypes = array('jpg','png','jpeg','gif','pdf');
     $target_dir = "uploads/";
-    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-    echo "$target_file";
-    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-    
-    // check if image file is a actual image or fake image
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - ".$check["mime"];
-    }else {
-        echo "<script> alter('File is not an image') </script>";
-    }
-
-    // check file size
-    if($_FILES["fileToUpload"]["size"] > 500000) {
-        echo "<script> alter('File is too big an image') </script>";
-    }
-
-    if(!in_array($imageFileType, $allowTypes)) {
-        echo "<script> alter('Sorry only JPG, JPEG, PNG & GIF files are allowed') </script>";
-    }
-
-    if(count($errors)  ==0) {
-        if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            $insert = $con -> query("INSERT INTO images (file_name) VALUES ('$target_file')");
-            if($insert) {
-                echo "succesfull";
-            }else {
-                echo "wth";
-            }
+    $target_file = "";
+    if(file_exists($_FILES["fileToUpload"]["name"])) {
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+        
+        // check if image file is a actual image or fake image
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image - ".$check["mime"];
         }else {
-            echo "sorry there was an error in uploading. Try again";
+            echo "<script> alter('File is not an image') </script>";
+        }
+
+        // check file size
+        if($_FILES["fileToUpload"]["size"] > 500000) {
+            echo "<script> alter('File is too big an image') </script>";
+        }
+
+        if(!in_array($imageFileType, $allowTypes)) {
+            echo "<script> alter('Sorry only JPG, JPEG, PNG & GIF files are allowed') </script>";
+        }
+
+        if(count($errors)  ==0) {
+            if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                $insert = $con -> query("INSERT INTO images (file_name) VALUES ('$target_file')");
+                if($insert) {
+                    echo "succesfull";
+                }else {
+                    echo "wth";
+                }
+            }else {
+                echo "sorry there was an error in uploading. Try again";
+            }
         }
     }
     
 
     $username = mysqli_real_escape_string($con, $_POST['landlord_name']);
     $location = mysqli_real_escape_string($con, $_POST['location']);
-    // $email = mysqli_real_escape_string($con, $_POST['email_id']);
-    // $pwd1 = mysqli_real_escape_string($con, $_POST['password1']);
-    // $pwd2 = mysqli_real_escape_string($con, $_POST['password2']);
-    // $mobile_number = mysqli_real_escape_string($con, $_POST['mobile_number']);
+    $email = mysqli_real_escape_string($con, $_POST['email_id']);
+    $pwd = mysqli_real_escape_string($con, $_POST['password']);
+    $mobile_number = mysqli_real_escape_string($con, $_POST['mobile_number']);
 
+    $appartment_name = mysqli_real_escape_string($con, $_POST['app_name']);
+    $appartment_number = mysqli_real_escape_string($con, $_POST['app_no']);
+    $appartment_location = mysqli_real_escape_string($con, $_POST['app_loc']);
+    $appartment_rent = mysqli_real_escape_string($con, $_POST['app_rent']);
+    $appartment_type = mysqli_real_escape_string($con, $_POST['app_type']);
+    $appartment_facilities = mysqli_real_escape_string($con, $_POST['app_fac']);
 
-    // if($pwd1 != $pwd2) {
-    //     echo "<script>alert('Passwords do not match') </script>";
-    //     echo "<script>window.location='../public/pages/landlord-reg.php'</script>";
-    // }
+    $user_check_query = "SELECT * FROM landlord WHERE landlord_name = '$username' OR email_id = '$email' LIMIT 1";
+    $result = mysqli_query($con, $user_check_query);
+    $user = mysqli_fetch_assoc($result);
 
-    // $user_check_query = "SELECT * FROM landlord WHERE landlord_name = '$username' OR email_id = '$email' LIMIT 1";
-    // $result = mysqli_query($con, $user_check_query);
-    // $user = mysqli_fetch_assoc($result);
+    if($user) { // if user with the mobile number or email_id exists then 
+        echo "<script> altert('The email id or password is already registered. Try loggin in instead')</script>";
+    }
 
-    // if($user) {
+    // insert data into landlord table as images table id is a foreign key referncing the mobile number of landlord table
+    if(count($errors) == 0) {
+        $password = md5($pwd1);
+        $appartment_id = uniqid($mobile_number);
+        $query = "INSERT INTO landlord (appartment_id, landlord_name, appartment_name, appartment_type, appartment_number
+        , appartment_rent, landlord_pwd, email_id, mobile_number, profile_pic, landlord_location, appartment_location, app_fac )
+                  VALUES('$appartment_id', '$username', '$appartment_name', '$appartment_type', '$appartment_number', '$appartment_rent', 
+        '$password', '$email', '$mobile_number', '$target_file', '$location', '$appartment_location', '$appartment_facilities' )";
+        echo "$query";
+        mysqli_query($con, $query);
+    }
 
-    // }
+    // for getting the appartment photos:
+    $statusMsg = $errorMsg = $insertValuesSQL = $errorUpload = $errorUploadType = ''; 
+    $file_names = array_filter($_FILES['files']['name']);
 
-    // if(count($errors) == 0) {
-    //     $password = md5($pwd1);
-    //     $uniq_id = uniqid($mobile_number);
+    if(!empty($file_names)) {
+        foreach($_FILES['files']['name'] as $key => $val) {
+            // file upload path
+            $file_name = basename($_FILES['files']['name'][$key]);
+            $targetFilePath =   $target_dir.$file_name;
 
-    //     $query = "INSERT INTO landlord (landlord_id, appartment_id, landlord_name, appartment_name, appartment_type, )
-    //               VALUES()";
+            // check whether file type is valid
+            $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
+            if(in_array($fileType, $allowTypes)) {
+                // upload file to server
+                if(move_uploaded_file($_FILES['files']['tmp_name'][$key], $targetFilePath)) {
+                    $insertValuesSQL .= "(\""."$file_name"."\"".", "."\""."$mobile_number"."\""."),";
+                }else {
+                    $errorUpload .= $_FILES['files']['name'][$key].' | ';
+                }
+            }else {
+                $errorUploadType .= $_FILES['files']['name'][$key].' | '; 
+            }
+        }
 
-    //     mysqli_query($con, $query);
-    // }
+        // Error message
+        $errorUpload = !empty($errorUpload)?'Upload Error: '.trim($errorUpload, ' | '):''; 
+        $errorUploadType = !empty($errorUploadType)?'File Type Error: '.trim($errorUploadType, ' | '):''; 
+        $errorMsg = !empty($errorUpload)?'<br/>'.$errorUpload.'<br/>'.$errorUploadType:'<br/>'.$errorUploadType;
+    
+        if(!empty($insertValuesSQL)) {
+            $insertValuesSQL = trim($insertValuesSQL, ',');
+            // insert image file name into the database
+            echo "$insertValuesSQL";
+            $insert = $con -> query("INSERT INTO images (file_name, app_ref) VALUES $insertValuesSQL");
+            if($insert){ 
+                $statusMsg = "Files are uploaded successfully.".$errorMsg; 
+            }else{ 
+                $statusMsg = "Sorry, there was an error uploading your file."; 
+            }
+        }else{ 
+            $statusMsg = "Upload failed! ".$errorMsg; 
+        }
+    
+    }else {
+        $statusMsg = "please select file to upload";
+    }
 
-
+     echo "$statusMsg";
  
 }
 
